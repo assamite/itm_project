05fa9004af1227469c553bbc0d1df7fb54e23072
Simo Linkola <slinkola@cs.helsinki.fi>
2014-11-04 23:10:21 +0200

    Removed another 5 bytes more from dec2.py

--- src/dec2.py
+++ src/dec2.py
@@ -1,2 +1,2 @@
-from struct import unpack
-open('curve1.dat','w').write("\n".join([str(int(i,2)-1100) for i in map("".join,zip(*[iter(bin(reduce(lambda x,y:(x<<8)+y,unpack('275B',open('test','r').read())))[2:])]*11))])+"\n")
+import struct
+open('curve1.dat','w').write("\n".join([str(int(i,2)-1100) for i in map("".join,zip(*[iter(bin(reduce(lambda x,y:(x<<8)+y,struct.unpack('275B',open('test','r').read())))[2:])]*11))])+"\n")

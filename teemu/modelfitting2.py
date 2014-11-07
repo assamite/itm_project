4557154290525d4591036b7c2c9264280394dccd
Teemu Pitkänen <teemu.pitkanen@helsinki.fi>
2014-11-07 13:12:35 +0200

    Tuning the model

--- teemu/modelfitting2.py
+++ teemu/modelfitting2.py
@@ -12,15 +12,16 @@ data = [float(x) for x in data]
 #data[63] = (data[62]+data[64])/2
 
 # Only optimize this type of model wrt data points 0...99
+data_all=data
 data=[data[x] for x in range(1000)]
 
 # The general model class used for optimization
 def fun(x, a, b, c, d, e, f):
-	val = np.array([(a-b*xi)*(np.abs(np.cos(c*xi + d))-1)+e for xi in x])
-	for i in range(len(x)):
-		if val[i] > f:
-			val[i] = f
-	return val
+        val = np.array([(a-b*xi)*(np.abs(np.cos(c*xi + d))-1)**2+e for xi in x])
+#        for i in range(len(x)):
+#                if val[i] > f:
+#                        val[i] = f
+        return val
 
 # Function with a rough approximation of the optimized parameters
 #def funopt(x):
@@ -33,20 +34,11 @@ def fun(x, a, b, c, d, e, f):
 # Optimization using scipy.opitmize.curve_fit
 ran = np.linspace(0,999,num=1000)
 popt, popcov = curve_fit(fun, ran, data, p0 = [1700, 12, 1.0/60, 0, 500, 50])
-
+ran2 = np.linspace(0,1999,num=2000)
 # Plot data and fitted function
-#y=funopt(ran)
-y=fun(ran,*popt)
+#y=funopt(ran2)
+y=fun(ran2,*popt)
 #data[63]=data63 # Just adding the anomaly in data back
-plt.plot(data)
+plt.plot(data_all)
 plt.plot(y);
 plt.show()
-
-# Counting and saving the differences between the fitted model and actual data
-difference = data - y
-rss = 0
-for i in range(len(difference)):
-	rss += difference[i]**2
-print("Residual sum of squares: ",rss)
-np.savetxt("d",difference,"%d")	
-

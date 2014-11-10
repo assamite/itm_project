5a1529c74f876558001a4d53c615ef2f12d3e13e
Teemu Pitkänen <teemu.pitkanen@helsinki.fi>
2014-11-10 15:43:50 +0200

    Cleaned d.py

--- teemu/d.py
+++ teemu/d.py
@@ -1,11 +1,10 @@
 import numpy as np
-r=open("r","r").read().split("\n")
+r= open("r","r").read().split("\n")
 r.pop()
 r=[float(x) for x in r]
-[a,b,c,d,e,f,g,h,i,j]=[-1.08, 1363.0, 1.0/60, 15.0, 1.09,-28.15,-24.33,0.27,-0.7,2.64]
 for n in range(2000):
 	if(n<1250):
-		r[n] = r[n]+(np.abs(a*n + b)) *(-1)* (np.abs(np.cos(c*n))-1)**2+d                
+		r[n]=r[n]-(np.abs(-1.08*n+1363))*(np.abs(np.cos(n*1.0/60))-1)**2+15                
 	else:
-		r[n] = r[n]+(e*(n-1250) + f) * ( np.abs( np.cos((2*c)*(n-1250) + g) + h ) +i ) + j
-np.savetxt("decompressed",r,"%.2f")
+		r[n]=r[n]+(1.09*(n-1250)-28.15)*(np.abs(np.cos((1.0/30)*(n-1250)-24.33)+0.27)-0.7)+2.64
+np.savetxt("curve1.dat",r,"%.2f")

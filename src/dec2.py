88a478ea772a4bc04c81ba9adb465fa8351cc224
Simo Linkola <slinkola@cs.helsinki.fi>
2014-11-12 11:52:21 +0200

    Added paleo to second return sections also. modified readme to contain data file ids.

--- src/dec2.py
+++ src/dec2.py
@@ -1,2 +1,10 @@
-import struct
-open('curve1.dat','w').write("\n".join([str(int(i,2)-1100) for i in map("".join,zip(*[iter(bin(reduce(lambda x,y:(x<<8)+y,struct.unpack('275B',open('test','r').read())))[2:])]*11))])+"\n")
+import struct,sys
+ir=xrange(255)
+dr=xrange(100)
+def f(path):
+    r=reduce(lambda x,y:x+'{:08b}'.format(y),struct.unpack('4000B',open(path).read()),"")
+    a=2000
+    s=["-" if x=='0' else '' for x in r[:a]]
+    i=map(lambda x:str(int("".join(x),2)),zip(*[iter(r[a:a*9])]*8))
+    d=map(lambda x:str(int("".join(x),2)),zip(*[iter(r[a*9:])]*7))
+    return "\n".join([x+y+"."+z for x,y,z in zip(s,i,d)])+"\n"
